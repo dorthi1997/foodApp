d1da773edabb30ef71fff544e0ea8f5003235013
Dorthishree <dorthishree5@gmail.com>
2017-07-22 13:45:13 +0530

    front page completed

---
+++ main.js
@@ -0,0 +1,45 @@
+ $('.loginbuttonn').on('click', function() {
+      $('.welcome_screen').addClass('hidden'); 
+     $('.signin').removeClass('hidden');
+ });
+ $('.front .btn').on('click', function() {
+     $('.signin').addClass('hidden');
+     $('.main').removeClass('hidden');
+ });
+var foodieApp = angular.module('foodieApp',[]);
+foodieApp.controller('mainController',function($scope) {
+$scope.restaurants = [{
+	name: 'Farzi Cafe',
+	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+	location: 'Connaught Place',
+	category: 'Casual Dining, Bar',
+	vote: '4.2',
+	cuisines: 'Modern Indian',
+	cost: '2200',
+	hours: '12 Noon to 1 AM (Mon-Sun)',
+	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+},
+       {
+	name: 'Farzi Cafe',
+	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+	location: 'Connaught Place',
+	category: 'Casual Dining, Bar',
+	vote: '4.2',
+	cuisines: 'Modern Indian',
+	cost: '2200',
+	hours: '12 Noon to 1 AM (Mon-Sun)',
+	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+},              
+      {
+	name: 'Farzi Cafe',
+	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+	location: 'Connaught Place',
+	category: 'Casual Dining, Bar',
+	vote: '4.2',
+	cuisines: 'Modern Indian',
+	cost: '2200',
+	hours: '12 Noon to 1 AM (Mon-Sun)',
+	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+},                       
+                     
+                     ]})
